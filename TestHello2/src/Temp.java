d2d68d551e6a83d8e1bc2bd356482d108206940a
alok <alok@ymail.com>
2019-08-25 13:16:11 +0530

    test project added

---
+++ TestHello2/src/Temp.java
@@ -0,0 +1,25 @@
+
+
+import java.util.ArrayList;
+
+public class Temp {
+	//
+	public static void main(String[] args) {
+		
+		stream();
+	}
+	
+	private static void stream() {
+	
+		ArrayList<Integer> list = new ArrayList<Integer>();
+	
+		list.add(4);
+		list.add(3);
+		list.add(2);
+		list.add(1);
+		list.add(7);
+		list.add(8);
+		
+		System.out.println("list >>:"+list);
+	}
+}
